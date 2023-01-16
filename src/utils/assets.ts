95fedc4809532053b7b5b94f538d3cf35e046d34
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-01-16 13:17:09 +0000

    Remove asset packaging

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -3,7 +3,6 @@ import util from "node:util";
 import fse from "fs-extra";
 import globCb from "glob";
 import { Config } from "./config";
-import { isPath } from "./path";
 
 const glob = util.promisify(globCb);
 
@@ -14,11 +13,7 @@ export async function copyAssets(
   const defaultFilesRe = /^(readme|licence|license)(|\.[a-z]+)$/i;
   const filePaths = await glob(`${cfg.projectDir}/**/*`, {
     nodir: true,
-    ignore: [
-      `${cfg.projectDir}/dist/**/*`,
-      `${cfg.projectDir}/**/*.ts`,
-      `${cfg.projectDir}/**/*.tsx`,
-    ],
+    ignore: [`${cfg.projectDir}/dist/**/*`],
   });
 
   for (const filePath of filePaths) {
@@ -26,23 +21,6 @@ export async function copyAssets(
     if (filePathRelativeToProject.match(defaultFilesRe)) {
       const outFilePath = path.join(opts.outDir, filePathRelativeToProject);
       await fse.copy(filePath, outFilePath);
-    }
-    if (isPath(cfg.projectBaseDir).parentOf(filePath)) {
-      for (const outBaseDir of opts.outBaseDirs) {
-        const filePathRetiveToBase = path.relative(
-          cfg.projectBaseDir,
-          filePath
-        );
-        const outFilePath = path.join(outBaseDir, filePathRetiveToBase);
-        console.log({
-          filePath,
-          filePathRetiveToBase,
-          projectBaseDir: cfg.projectBaseDir,
-          outBaseDir,
-          outFilePath,
-        });
-        await fse.copy(filePath, outFilePath);
-      }
     }
   }
 }
